1c6f69b7c56ba4bcd72fd52213e383d480e01b6f
Rui Anderson <ruiandersonsantos@gmail.com>
2017-10-23 16:57:08 -0200

    migrate refresh

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   urlBase: 'http://www.apiboleto.dtconsultoria.com.br/api',
   urlLogin: 'http://www.apiboleto.dtconsultoria.com.br/oauth/token',
   urlLogout: 'http://www.apiboleto.dtconsultoria.com.br/oauth/revoke_token',
-  client_Secret: 'q58YB7pBYRiRsO4q4nGJXAFXCCaTkHf4t4K9fdPK',
+  client_Secret: 'FFJ9XYhrOrXIZP6i7E70X5GkPNy6g36cUB5pEGEe',
   client_id: 2,
   grant_type: 'password',
   verbo_get: 'GET',
